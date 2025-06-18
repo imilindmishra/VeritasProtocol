896462cb82a6b44076cab4c160ecfe5ae1de2d8f
imilindmishra <milindmishra11@gmail.com>
2025-06-18 08:24:51 +0530

    address update

--- contracts/scripts/verify.js
+++ contracts/scripts/verify.js
@@ -1,6 +1,6 @@
 // File: scripts/verify.js
 async function main() {
-  const marketAddress = "0x0c47D57113F74D4dd4c46ED35D932FFBdF804C36"; 
+  const marketAddress = "0x11BC4A9C06A89b979Eb430a22D9fCC2F4288a355"; 
   const market = await ethers.getContractAt("BettingMarket", marketAddress);
 
   console.log("Question:", await market.question());
